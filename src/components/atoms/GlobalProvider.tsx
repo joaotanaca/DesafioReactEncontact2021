f350ffa6aebe288a52d930435023fe6c0084ea22
Tanaca <jtanacar@gmail.com>
2021-10-07 19:34:31 -0300

    reduce bundle's framer-motion

--- src/components/atoms/GlobalProvider.tsx
+++ src/components/atoms/GlobalProvider.tsx
@@ -1,16 +1,21 @@
+import { LazyMotion } from "framer-motion";
 import React from "react";
 import { TaskFramerProvider } from "src/context/taskFramer";
 import GlobalStyle from "src/styles/global.style";
 import { ThemeProvider } from "styled-components";
 import theme from "../../styles/theme";
 
+const loadFeatures = () => import("framer-motion").then((res) => res.domMax);
+
 const GlobalProvider: React.FC = ({ children }) => (
-    <ThemeProvider theme={theme}>
-        <TaskFramerProvider>
-            <GlobalStyle />
-            {children}
-        </TaskFramerProvider>
-    </ThemeProvider>
+    <LazyMotion features={loadFeatures}>
+        <ThemeProvider theme={theme}>
+            <TaskFramerProvider>
+                <GlobalStyle />
+                {children}
+            </TaskFramerProvider>
+        </ThemeProvider>
+    </LazyMotion>
 );
 
 export default GlobalProvider;
